5c55e9db4f96e6aafc8f8de3ba5e77c797bdae95
Jii-Yeong <tj5134@naver.com>
2024-03-03 18:03:21 +0900

    fix: PostListItem 컴포넌트 스타일 수정

--- components/item/PostListItem.tsx
+++ components/item/PostListItem.tsx
@@ -2,6 +2,7 @@
 
 import {PostListItemType} from '@/types/post.types';
 import Image from 'next/image';
+import Link from 'next/link';
 
 type PostListItemProps = {
   item: PostListItemType;
@@ -10,12 +11,20 @@ type PostListItemProps = {
 export default function PostListItem({item}: PostListItemProps) {
   const thumbnail = require(`@/data/posts/thumbnail/${item.thumbnail}.png`);
   return (
-    <div className="flex flex-row bg-[#ffffff] rounded-lg p-6">
+    <Link
+      href={`/post/${item.id}`}
+      className="flex flex-row bg-[#ffffff] rounded-lg p-6">
       <div className="flex flex-col gap-y-2 flex-1">
         <p className="text-[#7b71be]">{item.category}</p>
         <p className="text-xl">{item.title}</p>
       </div>
-      <Image src={thumbnail} alt="post-thumbnail" width={128} height={80} />
-    </div>
+      <Image
+        className="my-0"
+        src={thumbnail}
+        alt="post-thumbnail"
+        width={128}
+        height={80}
+      />
+    </Link>
   );
 }
